335b29e5f7ead08392f36605ce9f2b8055e236a0
Setenta30 <gonzalo_britez@hotmail.com>
2024-04-23 01:28:13 -0300

    onDestroy

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,24 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { StudentsService } from '../services/students.service';
 import { Student } from '../models/student';
-import { Observable, Subject, delay, map, takeUntil, tap } from 'rxjs';
+import { Observable, Subject, map, takeUntil } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrl: './students.component.css'
+  styleUrls: ['./students.component.css']
 })
-export class StudentsComponent {
-  students$!: Observable<Array<string>>;
+export class StudentsComponent implements OnInit, OnDestroy {
+  onDestroy$ = new Subject<void>();
+  students$!: Observable<string[]>;
 
   constructor(
     public studentsService: StudentsService
   ) { }
 
   ngOnInit() {
-    this.students$ = this.studentsService.getStudents()
-      .pipe(
-        map(students => students.map(student => `${student.first_name} ${student.last_name}`)),
-      )
+    this.students$ = this.studentsService.getStudents().pipe(
+      takeUntil(this.onDestroy$),
+      map(students => students.map(student => `${student.first_name} ${student.last_name}`)),
+    );
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
   }
 }
